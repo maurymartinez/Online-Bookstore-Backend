2f028a920773261d41399b1f88511a60197cb126
maury <maury.martinez@tutanota.com>
2025-06-18 17:03:10 +0200

    Implement POST /api/auth/login

--- src/main/java/com/quasas/bookstore/security/api/controller/AuthController.java
+++ src/main/java/com/quasas/bookstore/security/api/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.quasas.bookstore.security.api.controller;
 
+import com.quasas.bookstore.security.api.dto.LoginRequest;
 import com.quasas.bookstore.security.api.dto.RegisterRequest;
 import com.quasas.bookstore.security.application.service.AuthService;
 import jakarta.validation.Valid;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -24,5 +27,11 @@ public class AuthController {
     public ResponseEntity<Void> register(@Valid @RequestBody RegisterRequest request) {
         authService.register(request.email(), request.password(), request.name());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody @Valid LoginRequest request) {
+        String token = authService.login(request.email(), request.password());
+        return ResponseEntity.ok(Map.of("access_token", token));
     }
 }
