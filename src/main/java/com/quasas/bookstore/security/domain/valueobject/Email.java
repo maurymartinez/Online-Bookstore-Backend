b4c0bfc74640fc7a4ab0f15a38d4e3e003e49cf4
maury <maury.martinez@tutanota.com>
2025-06-18 11:04:52 +0200

    Implement Email validation

--- src/main/java/com/quasas/bookstore/security/domain/valueobject/Email.java
+++ src/main/java/com/quasas/bookstore/security/domain/valueobject/Email.java
@@ -1,5 +1,9 @@
 package com.quasas.bookstore.security.domain.valueobject;
 
 public record Email(String value) {
-
+    public Email {
+        if (value == null || !value.matches("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+    }
 }
