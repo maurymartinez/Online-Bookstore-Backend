88d880c025aab3f22b37fce741f6770765d8e624
maury <maury.martinez@tutanota.com>
2025-06-18 14:18:31 +0200

    Add fields validations to RegisterRequest

--- src/main/java/com/quasas/bookstore/security/api/dto/RegisterRequest.java
+++ src/main/java/com/quasas/bookstore/security/api/dto/RegisterRequest.java
@@ -1,4 +1,18 @@
 package com.quasas.bookstore.security.api.dto;
 
-public record RegisterRequest(String email, String password, String name) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @Email(message = "Invalid email format")
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        @NotBlank(message = "Password is required")
+        String password,
+
+        @NotBlank(message = "Name is required")
+        String name) {
 }
