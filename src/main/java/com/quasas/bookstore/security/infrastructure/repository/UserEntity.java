4773a3efa4b43b9c413480542863d5689fd352af
maury <maury.martinez@tutanota.com>
2025-06-18 13:58:30 +0200

    Implement infra UserEntity

---
+++ src/main/java/com/quasas/bookstore/security/infrastructure/repository/UserEntity.java
@@ -0,0 +1,34 @@
+package com.quasas.bookstore.security.infrastructure.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+}
