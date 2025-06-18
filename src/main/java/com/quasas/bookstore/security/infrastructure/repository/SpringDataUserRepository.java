2e593ee5aa7f7ac22d71becfabefe22dbeca4a08
maury <maury.martinez@tutanota.com>
2025-06-18 16:38:02 +0200

    Implement UserRepository.findByEmail

--- src/main/java/com/quasas/bookstore/security/infrastructure/repository/SpringDataUserRepository.java
+++ src/main/java/com/quasas/bookstore/security/infrastructure/repository/SpringDataUserRepository.java
@@ -2,8 +2,10 @@ package com.quasas.bookstore.security.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataUserRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
