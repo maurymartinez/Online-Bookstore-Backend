2700ab0986e0281f94df203e871f65e9a68da889
maury <maury.martinez@tutanota.com>
2025-06-18 16:37:38 +0200

    Add UserRepository.findByEmail

--- src/main/java/com/quasas/bookstore/security/domain/UserRepository.java
+++ src/main/java/com/quasas/bookstore/security/domain/UserRepository.java
@@ -2,7 +2,10 @@ package com.quasas.bookstore.security.domain;
 
 import com.quasas.bookstore.security.domain.valueobject.Email;
 
+import java.util.Optional;
+
 public interface UserRepository {
     void save(User user);
     boolean existsByEmail(Email email);
+    Optional<User> findByEmail(Email email);
 }
